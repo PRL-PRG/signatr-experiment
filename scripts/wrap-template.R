ecc596bdea8afcd35991287d9a6153de6058a045
Pierre Donat-Bouillud <pierre.donat.bouillud@gmail.com>
2021-10-26 16:38:20 +0200

    Convert to targets pipeline

--- scripts/wrap-template.R
+++
@@ -1,9 +0,0 @@
-db_path <- file.path(Sys.getenv("RUNR_CWD"), "db")
-record::open_db(db_path, create= if(!dir.exists(db_path)) TRUE)
-
-argtracer::trace_args(code={
-  .BODY.
-})
-
-record::size_db()
-record::close_db()
