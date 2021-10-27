a35b7331a6d8d08f7110bfefa330beb8fe3b58e4
Pierre Donat-Bouillud <pierre.donat.bouillud@gmail.com>
2021-10-27 15:19:21 +0200

    Targets pipeline

---
+++ R/functions.R
@@ -0,0 +1,129 @@
+install_cran_packages <- function(packages_to_install,
+                                  lib = NULL,
+                                  destdir = NULL,
+                                  mirror = "https://cloud.r-project.org/") {
+  options(repos = mirror)
+
+  requested <- packages_to_install
+
+  installed <- installed.packages(lib.loc = lib)[, 1]
+  missing <- setdiff(requested, installed)
+
+  message("Installing ",
+          length(missing),
+          " packages from ",
+          mirror,
+          " into ",
+          lib)
+
+  if (length(missing) > 0) {
+    if (!is.null(destdir) &&
+        !dir.exists(destdir))
+      dir.create(destdir, recursive = TRUE)
+    if (!is.null(lib) &&
+        !dir.exists(lib))
+      dir.create(lib, recursive = TRUE)
+  }
+
+  # set package installation timeout
+  Sys.setenv(
+    `_R_INSTALL_PACKAGES_ELAPSED_TIMEOUT_` = Sys.getenv("_R_INSTALL_PACKAGES_ELAPSED_TIMEOUT_", "5000")
+  )
+
+  callr::r(
+    function(x) {
+      install.packages(
+        x,
+        dependencies = TRUE,
+        INSTALL_opts = c(
+          "--example",
+          "--install-tests",
+          "--with-keep.source",
+          "--no-multiarch"
+        ),
+        Ncpus = floor(.9 * parallel::detectCores())
+      )
+    },
+    list(missing),
+    libpath = lib_path,
+    arch = "R-dyntrace/bin/R",
+    show = TRUE,
+    env = r_envir
+  )
+
+  installed <- installed.packages(lib.loc = lib)[, 1]
+  successful_installed <- intersect(installed, requested)
+
+  # Extract the sources from the missing ones that successfully installed
+  installed_missing <- intersect(successful_installed, missing)
+
+  if (!is.null(destdir)) {
+    archives <-
+      list.files(destdir, pattern = "\\.tar\\.gz$", full.names = TRUE)
+    for (package in installed_missing) {
+      destfiles <- grep(package, archives, value = TRUE, fixed = TRUE)
+      if (length(destfiles) == 0) {
+        next
+
+      }
+      destfile <- destfiles[[1]] # there should be only one anyway
+      pkgdir <- file.path(destdir, package)
+      if (dir.exists(pkgdir)) {
+        warning("Destination directory for extracting exists ",
+                pkgdir,
+                "\n")
+      }
+      message("Extracting", destfile , " to ", pkgdir, "\n")
+      utils::untar(destfile, exdir = destdir)
+      file.remove(destfile) # remove the archive so that it does not interfere with newer versions
+      # It does not remove the tar of the dependencies though...
+    }
+  }
+
+
+  successful_installed
+}
+
+extract_code_from_package <-
+  function(package, lib_path, output_path) {
+    # extract codes, should return the path of all extracted files...
+    # so maybe do it with 2 functions, one for the examples, and one for the rest
+    # to be able to feed the concatenation for the examples
+    runr::extract_package_code(package,
+                               pkg_dir = file.path(lib_path, package),
+                               output_dir = output_path) %>%
+      mutate(file = file.path(normalizePath(output_path, mustWork = TRUE), file)) %>%
+      pull(file)
+  }
+
+
+concatenate_examples <- function(package, examples) {
+  # This takes all the extracted examples of the package and generates one example file
+  # This is in order to speed up the tracing
+}
+
+trace_file <- function(file_path, lib_path, output_path) {
+  # Put the right arch, the right libPaths and so on
+  print(file_path)
+  callr::r(
+    function(x, y) {
+      argtracer::trace_file(x, file.path(y, basename(x)))
+    },
+    list(file_path, output_path),
+    libpath = lib_path,
+    arch = "R-dyntrace/bin/R",
+    show = TRUE,
+    env = r_envir
+  )
+  file.path(output_path, basename(file_path))
+}
+
+merge_db <- function(db_paths, output_path) {
+  db_name = "cran_db"
+  sxpdb::create_db(db_name)
+  for(db_path in db_paths) {
+    sxpdb::merge_db(db_path)
+  }
+  sxpdb::close_db()
+  file.path(output_path, db_name)
+}
---
+++ _targets.R
@@ -0,0 +1,85 @@
+library(targets)
+library(tarchetypes)
+library(future)
+library(future.callr)
+source("R/functions.R")
+options(tidyverse.quiet = TRUE)
+options(future.wait.timeout = 60 * 60) # do not allow more than 1h for each task
+
+lib_path = "library-local"
+output_path = "data/extracted-code"
+r_envir = c(callr::rcmd_safe_env(),
+           "R_KEEP_PKG_SOURCE"=1,
+           "R_ENABLE_JIT"=0,
+           "R_COMPILE_PKGS"=0,
+           "R_DISABLE_BYTECODE"=1)
+
+plan(callr)
+
+# If you want to see the error messages, you can easily do it
+# by looking at the result of target"_err"
+tar_target_resilient <- function(name, command, pattern, ...) {
+  tname <- deparse(substitute(name))
+  tname_err <- paste0(tname, "_err")
+  sym_err <- parse(text = tname_err)[[1]]
+  wrapped_command <- substitute(tryCatch(COMMAND, error = function(e) e), list(COMMAND = substitute(command)))
+  # Or Filter if we do not want to use purrr
+  compact_command <- substitute(purrr::discard(TNAME_ERR, function(v) inherits(v, "error")), list(TNAME_ERR = sym_err))
+  tpattern <- substitute(map(TNAME_ERR), list(TNAME_ERR = sym_err))
+  list(
+    tar_target_raw(tname_err, wrapped_command, pattern = substitute(pattern), iteration = "list", ...),
+    tar_target_raw(tname, command = compact_command, pattern = tpattern)
+  )
+}
+
+tar_option_set(
+  packages = c("readr", "covr", "magrittr", "dplyr"),
+  #imports = c("sxpdb", "argtracer"),
+  #error = "continue" # always continue by default
+)
+
+list(
+  tar_target(
+    packages_file,
+    "data/packages.txt",
+    format = "file"
+  ),
+  tar_target(
+    packages_to_install,
+    unique(trimws(read_lines(packages_file)))
+  ),
+  tar_target(
+    packages_to_run,
+    install_cran_packages(packages_to_install, lib_path, NULL),
+    deployment = "main"
+  ),
+
+  tar_target(
+    extracted_files,
+    extract_code_from_package(packages_to_run, lib_path, output_path),
+    #deployment = "main",
+    format = "file",
+    pattern = map(packages_to_run)
+  ),
+
+  # We need to flatten all the files (from one vector of vectors, one per package, to only one vector)
+  tar_target(
+    individual_files,
+    extracted_files,
+  ),
+
+  tar_target(
+    traced_results,
+    trace_file(individual_files, lib_path, output_path),
+    format = "file", # what if it takes ages to hash all the databases?
+    # then we can just return the paths but ask targets not to look at the files themselves and assume everything is fine
+    # or use a time cue instead of using a hash one?
+    pattern = map(individual_files)
+  ),
+
+  tar_target(
+    merged_db,
+    merge_db(traced_results, output_path),
+    format = "file"
+  )
+)
