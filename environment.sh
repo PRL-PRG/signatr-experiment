28840f514ae02a285a295bd6ad781d32d301b423
programLyrique <pierre.donat.bouillud@gmail.com>
2021-11-24 12:00:34 -0500

    Environment variables to source for R-dyntrace and vanilla R

--- environment.sh
+++ environment.sh
@@ -1,10 +1,6 @@
-R_BIN=$(readlink -e R-dyntrace/bin)
+R_BIN=$(readlink -e R-4.0.2/bin)
 
 export PATH=$R_BIN:$PATH
 export R_LIBS=$(readlink -m library-local)
-export R_KEEP_PKG_SOURCE=1
-export R_ENABLE_JIT=0
-export R_COMPILE_PKGS=0
-export R_DISABLE_BYTECODE=1
 
 [ -d "$R_LIBS" ] || mkdir -p "$R_LIBS"
