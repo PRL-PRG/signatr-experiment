d23318e85d3596511d78379572842ac45107f17b
programLyrique <pierre.donat.bouillud@gmail.com>
2022-03-09 09:17:36 -0500

    library-local -> library for env-*-dbg.sh

--- env-4.0.2-dbg.sh
+++ env-4.0.2-dbg.sh
@@ -1,6 +1,6 @@
 R_BIN=$(readlink -e R-4.0.2-dbg/bin)
 
 export PATH=$R_BIN:$PATH
-export R_LIBS=$(readlink -m library-local)
+export R_LIBS=$(readlink -m library)
 
 [ -d "$R_LIBS" ] || mkdir -p "$R_LIBS"
