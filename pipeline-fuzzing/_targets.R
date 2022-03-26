3866c65c7961af18cecb5abeea9bd6c6ce9e00fe
Filip Krikava <krikava@gmail.com>
2022-03-26 08:38:45 -0400

    Updates to pipeline

--- pipeline-fuzzing/_targets.R
+++ pipeline-fuzzing/_targets.R
@@ -1,10 +1,10 @@
-library(tarchetypes)
-library(targets)
-library(tibble)
+library(dplyr)
 library(future)
 library(future.callr)
 library(generatr)
-library(dplyr)
+library(tarchetypes)
+library(targets)
+library(tibble)
 library(withr)
 
 
@@ -23,18 +23,23 @@ R_ENVIR <- c(
     "R_DISABLE_BYTECODE" = 1
 )
 
-# maximum time allowed for a program to run in seconds
-TIMEOUT <- 5 * 60
 OUT_DIR <- Sys.getenv("OUT_DIR", "out")
 LIB_DIR <- Sys.getenv("LIB_DIR", file.path(OUT_DIR, "library"))
 DB_DIR <- Sys.getenv("DB_DIR", "/mnt/ocfs_vol_00/cran_db-5/")
 BUDGET <- 3000
+# maximum time allowed for a function call
+TIMEOUT_MS <- 60 * 1000
 
 # packages to test
-CORPUS <- c("stringr")
+# CORPUS <- c("stringr", "dplyr")
+CORPUS <-
+    readLines("/mnt/ocfs_vol_00/signatr/packages-94.txt") %>%
+    trimws(which = "both") %>%
+    unique()
 
 tar_option_set(
-    packages = c("dplyr", "generatr", "runr", "sxpdb"),
+    packages = c("dplyr", "generatr", "runr", "sxpdb", "withr"),
+    format = "qs"
 )
 
 plan(callr)
@@ -72,31 +77,43 @@ list(
     tar_target(
         functions,
         {
-            runr::metadata_functions(packages$package) %>%
-                filter(exported, !is_s3_dispatch, !is_s3_method)
+            runr::metadata_functions(packages$package, lib_loc = LIB_DIR) %>%
+                dplyr::filter(exported, !is_s3_dispatch, !is_s3_method) %>%
+                dplyr::filter(!grepl("...", params, fixed = TRUE))
         },
         pattern = map(packages)
     ),
-    tar_target(
-        functions_,
-        dplyr::bind_rows(functions)
-    ),
-    tar_target(
+    # tar_group_by(
+    #     functions_,
+    #     dplyr::bind_rows(functions),
+    #     pkg_name, fun_name
+    # ),
+    tar_group_by(
         origins_db,
         {
             value_db <- sxpdb::open_db(DB_DIR)
             sxpdb::view_origins_db(value_db) %>%
-                as_tibble
-        }
+                as_tibble %>%
+                semi_join(functions, by = c("pkg" = "pkg_name", "fun" = "fun_name"))
+        },
+        pkg, fun
     ),
     tar_target(
         meta_db,
         {
-            value_db <- sxpdb::open_db(DB_DIR)
-            sxpdb::view_meta_db(value_db) %>%
-                as_tibble
+            # value_db <- sxpdb::open_db(DB_DIR)
+            # sxpdb::view_meta_db(value_db) %>%
+            #     as_tibble
+            NULL
         }
     ),
+    # tar_target(
+    #     t,
+    #     {
+    #         list(o=origins_db, f=functions_)
+    #     },
+    #     pattern = map(origins_db, functions_)
+    # ),
     tar_target(
         run_existing,
         {
@@ -104,35 +121,42 @@ list(
             # run_existing / run_fuzz
             # ideally using another pattern which will iterate over the
             # different generators
+            # TODO: what if origins_db is empty?
+            pkg_name <- origins_db$pkg[1]
+            fun_name <- origins_db$fun[1]
             value_db <- sxpdb::open_db(DB_DIR)
             generator <- create_existing_args_generator(
-                functions_$pkg_name,
-                functions_$fun_name,
+                pkg_name,
+                fun_name,
                 value_db,
                 origins_db
             )
             runner <- runner_start()
             withr::defer(runner_stop(runner), envir = runner)
-            runner_fun <- create_fuzz_runner(DB_DIR, runner)
+            runner_fun <- create_fuzz_runner(DB_DIR, runner, timeout_ms = TIMEOUT_MS)
 
             fuzz(
-                functions_$pkg_name,
-                functions_$fun_name,
+                pkg_name,
+                fun_name,
                 generator = generator,
                 runner = runner_fun,
                 quiet = FALSE
-            )
+            ) %>%
+                mutate(pkg_name = pkg_name, fun_name = fun_name)
 
         },
-        pattern = map(functions_)
+        pattern = map(origins_db),
+        error = "continue"
     ),
     tar_target(
         run_fuzz,
         {
+            pkg_name <- origins_db$pkg[1]
+            fun_name <- origins_db$fun[1]
             value_db <- sxpdb::open_db(DB_DIR)
             generator <- create_fd_args_generator(
-                functions_$pkg_name,
-                functions_$fun_name,
+                pkg_name,
+                fun_name,
                 value_db,
                 origins_db,
                 meta_db = NULL,
@@ -140,17 +164,18 @@ list(
             )
             runner <- runner_start()
             withr::defer(runner_stop(runner), envir = runner)
-            runner_fun <- create_fuzz_runner(DB_DIR, runner)
+            runner_fun <- create_fuzz_runner(DB_DIR, runner, timeout_ms = TIMEOUT_MS)
 
             fuzz(
-                functions_$pkg_name,
-                functions_$fun_name,
+                pkg_name,
+                fun_name,
                 generator = generator,
                 runner = runner_fun,
-                quiet = FALSE
-            )
+                quiet = TRUE
+            ) %>%
+                mutate(pkg_name = pkg_name, fun_name = fun_name)
         },
-        pattern = map(functions_),
+        pattern = map(origins_db),
         error = "continue"
     )
 )
